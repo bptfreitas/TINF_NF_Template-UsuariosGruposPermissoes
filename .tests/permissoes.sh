d21daf7c407b63ed05fdb594375492b28bb582af
Bruno <bruno.freitas@cefet-rj.br>
2024-10-22 23:19:22 -0300

    Formatting pretty messages

--- .tests/permissoes.sh
+++ .tests/permissoes.sh
@@ -2,7 +2,7 @@
 
 if [[ ! -d /srv ]]; then
 
-	echo "Pasta /srv nao existe!"
+	echo -e "\nERRO: Pasta /srv nao existe!"
 	exit 1
 	
 fi
@@ -10,40 +10,38 @@ fi
 permission="`sudo ls -la / | grep srv | awk '{ print $1 }'`"
 
 if [[ "$permission" != "drwxr-x--x" ]]; then 
-	echo "Erro de permissoes em /srv! "
-	exit 1
+	echo -e "\nERRO: permissoes erradas em /srv! "
+	exit 2
 fi
 
 
 if [[ ! -d /srv/alunos ]]; then
 
-	echo "Pasta /srv/alunos nao existe!"
-	exit 1
+	echo -e "\nERRO: Pasta /srv/alunos nao existe!"
+	exit 3
 	
 fi
 
 permission="`sudo ls -la /srv | grep alunos | awk '{ print $1 }'`"
 
 if [[ "$permission" != "drwxrwx---" ]]; then 
-	echo "Erro de permissoes em /srv/alunos! "
-	exit 1
+	echo -e "\nERRO: permissoes erradas em /srv/alunos! "
+	exit 4
 fi
 
 
 if [[ ! -d /srv/professores ]]; then
 
-	echo "Pasta /srv/alunos nao existe!"
-	exit 1
+	echo -e "\nERRO: Pasta /srv/professores nao existe!"
+	exit 5
 	
 fi
 
 permission="`sudo ls -la /srv | grep professores | awk '{ print $1 }'`"
 
 if [[ "$permission" != "drwxrwx---" ]]; then 
-	echo "Erro de permissoes em /srv/alunos! "
-	exit 1
+	echo - "\nERRO: permissoes erradas em /srv/professores! "
+	exit 6
 fi
-
-
 
 exit 0
