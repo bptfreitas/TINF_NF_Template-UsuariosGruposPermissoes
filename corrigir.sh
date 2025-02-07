8c591536a94599e1998481fce392f433dc69aace
Bruno <bruno.freitas@cefet-rj.br>
2025-02-07 15:46:11 -0300

    Added specific tagging

--- corrigir.sh
+++ corrigir.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 container='usuarios-grupos-permissoes'
-tag='dev'
+
+if [[ "$STUDENT_TAG" != "" ]]; then 
+	tag="$STUDENT_TAG"
+else 
+	tag='dev'
+fi
 
 docker_dir="`which docker`"
 
